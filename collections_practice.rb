c48eed03630d5446fa03f0d4ec76b5aae76fc8ed
Diogo Ide <diogoide@gmail.com>
2018-04-07 21:07:32 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,7 +42,7 @@ end
 
 def add_s(array)
   #array.collect.with_index do |word, index|
-  [1,2].each_with_index.collect  do |word, index|
+  array.each_with_index.collect  do |word, index|
     if index == 1
        word
     else
